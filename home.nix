bd83d59054468493f2380b4f7c6521069ceb0faf
C Warren Dale <adventureonthehighseas@gmail.com>
2025-10-30 18:33:49 -0500

    add home-manager executable to home-manager config

--- home.nix
+++ home.nix
@@ -15,7 +15,8 @@ in
   home.packages = with pkgs; [
     # here is some command line tools I use frequently
     # feel free to add your own or remove some of them
-
+   
+    home-manager
     neofetch
     ranger
     ncdu
