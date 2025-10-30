60561694093419061e076d07e56aec2bf282802a
C Warren Dale <adventureonthehighseas@gmail.com>
2025-10-30 11:47:41 -0500

    Neovim!

---
+++ nvim/lua/plugins/haunt.lua
@@ -0,0 +1,11 @@
+return {
+  "adigitoleo/haunt.nvim",
+  opts = {
+        window = {
+            width_frac = 0.7,
+            winblend = 25,
+            title_pos = "center",
+        }
+    },
+    cmd = { "HauntTerm", "HauntMan", "HauntHelp" }
+}
